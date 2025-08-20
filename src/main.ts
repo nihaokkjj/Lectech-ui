465c9e14952d97b457143eaedda31039d8815982
nihaokkjj <sai_886@qq.com>
2025-08-21 06:11:29 +0800

    njy

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,11 @@
 import { createApp } from 'vue'
-<<<<<<< HEAD
-import './assets/styles/style.css'
-import App from './App.vue'
 
-import router from './router/index.js'
-
-
-=======
 
 import './style.scss'
 import './assets/styles/style.css'
 import router from './router/index.ts'
 import App from './App.vue'
 
-
->>>>>>> contributor/main
 const app = createApp(App)
 
 app.use(router)
